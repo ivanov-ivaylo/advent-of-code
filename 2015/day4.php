2839779c15ebe4a34cd1ad167f62da9361da50e8
Ivaylo Ivanov <ivaylo.ivanov@atia.com>
2023-12-02 18:54:03 +0200

    day 3 and day 4

---
+++ 2015/day4.php
@@ -0,0 +1,16 @@
+<?php
+
+$input = "ckczppom";
+
+
+for ($i = 0; $i < 10000000; $i++) {
+    $key = $input . $i;
+    
+    $md5 = md5($key);
+    
+    //echo $key . "/" . $md5 . "\n";
+    if (strpos($md5, '000000') === 0) {
+        echo $i;
+        break;
+    } 
+}
