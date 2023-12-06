b9a6331cacf4bb950b8d302e90cd36ba7cf2f4b4
Ivaylo Ivanov <ivaylo.ivanov@outdoorsy.co>
2023-12-06 14:11:05 +0200

    day 3

--- 2022/day03/day3.go
+++ 2022/day03/day3.go
@@ -11,6 +11,8 @@ type Rucksack struct {
 	Section1        string
 	Section2        string
 	SameItems       []string
+	Badge           string
+	BadgePriority   int
 	HighestPriority int
 }
 
@@ -26,28 +28,57 @@ func parseInput(inputData []byte) []Rucksack {
 	return result
 }
 
+func getPriority(letter int) int {
+	score := letter
+	if int(letter) < 97 {
+		score = int(letter) - 65 //A
+		score += 27
+	} else {
+		score = int(letter) - 97 //a
+		score += 1
+	}
+
+	return score
+}
+
 func findSameChars(sacks []Rucksack) {
 
 	for i := 0; i < len(sacks); i++ {
 		for _, letter := range sacks[i].Section1 {
 			if strings.Contains(sacks[i].Section2, string(letter)) {
 				if !slices.Contains(sacks[i].SameItems, string(letter)) {
-					score := int(letter)
-					if int(letter) < 97 {
-						score = int(letter) - 65 //A
-						score += 27
-					} else {
-						score = int(letter) - 97 //A
-						score += 1
-					}
-
 					sacks[i].SameItems = append(sacks[i].SameItems, string(letter))
-					sacks[i].HighestPriority = score
+					sacks[i].HighestPriority = getPriority(int(letter))
 				}
 			}
 		}
 
 	}
+}
+
+func findBadgeForRucksack(sacks []Rucksack) int {
+
+	res := 0
+	for i := 0; i < len(sacks); i += 3 {
+		item1 := sacks[i].Section1 + sacks[i].Section2
+		item2 := sacks[i+1].Section1 + sacks[i+1].Section2
+		item3 := sacks[i+2].Section1 + sacks[i+2].Section2
+
+		for _, item := range item1 {
+			if strings.Contains(item2, string(item)) && strings.Contains(item3, string(item)) {
+				sacks[i].Badge = string(item)
+				sacks[i+1].Badge = string(item)
+				sacks[i+2].Badge = string(item)
+				sacks[i].BadgePriority = getPriority(int(item))
+				sacks[i+1].BadgePriority = getPriority(int(item))
+				sacks[i+2].BadgePriority = getPriority(int(item))
+				res += getPriority(int(item))
+				break
+			}
+		}
+	}
+
+	return res
 }
 
 func Part1(inputData []byte) int {
@@ -64,7 +95,11 @@ func Part1(inputData []byte) int {
 }
 
 func Part2(inputData []byte) int {
-	return 0
+
+	sacks := parseInput(inputData)
+	findSameChars(sacks)
+
+	return findBadgeForRucksack(sacks)
 }
 
 func main() {
@@ -76,6 +111,6 @@ func main() {
 		return
 	}
 
-	fmt.Println(Part1(inputData))
-	//fmt.Println(Part2(inputData))
+	//fmt.Println(Part1(inputData))
+	fmt.Println(Part2(inputData))
 }
