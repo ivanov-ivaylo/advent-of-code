7474e5138a864ea19560937b419f82a5889cfa3b
Ivaylo Ivanov <ivaylo.ivanov@outdoorsy.co>
2023-12-12 14:54:32 +0200

    add day 7 2022

--- 2022/day07/day7.go
+++ 2022/day07/day7.go
@@ -11,31 +11,32 @@ type File struct {
 	isFile   bool
 	Name     string
 	Size     int
-	Parent   string
+	Parent   *File
 	Children map[string]File
 }
 
-func parseInput(inputData []byte) map[string]File {
+func parseInput(inputData []byte) (File, error) {
 	inputDataRows := strings.Split(string(inputData), "\n")
 	var dataRow string
-	var currentFolder string
-	var prevFolder string
 	var lsItem string
-	result := map[string]File{}
-	result["/"] = File{isFile: false, Name: "/", Children: map[string]File{}}
-	currentFolder = "/"
+	var currentFolder *File
+	result := File{isFile: false, Name: "/", Children: map[string]File{}}
+	currentFolder = &result
 	for len(inputDataRows) > 0 {
 		dataRow, inputDataRows = inputDataRows[0], inputDataRows[1:]
 		if string(dataRow[0]) == "$" {
 			command := dataRow[2:4]
+			currentName := strings.TrimSpace(dataRow[4:])
 			if command == "cd" {
-				if dataRow[4:] == ".." {
-					currentFolder = result[result[currentFolder].Parent].Name
+				if currentName == ".." {
+					currentFolder = currentFolder.Parent
+				} else if currentName == "/" {
+					continue
 				} else {
-					prevFolder = currentFolder
-					currentFolder = strings.TrimSpace(dataRow[4:])
-					if _, ok := result[currentFolder]; !ok {
-						result[currentFolder] = File{isFile: false, Name: currentFolder, Parent: prevFolder, Children: map[string]File{}}
+					if cFile, ok := currentFolder.Children[currentName]; ok {
+						currentFolder = &cFile
+					} else {
+						return result, fmt.Errorf("Unknown folder")
 					}
 				}
 			} else if command == "ls" {
@@ -48,33 +49,117 @@ func parseInput(inputData []byte) map[string]File {
 					}
 					if string(lsItem[0:3]) == "dir" {
 						currDir := File{isFile: false, Name: lsItem[4:], Parent: currentFolder, Children: map[string]File{}}
-						result[currentFolder].Children[lsItem[4:]] = currDir
+						currentFolder.Children[lsItem[4:]] = currDir
 					} else {
 						parts := strings.Split(lsItem, " ")
 						currFile := File{isFile: true, Name: parts[1], Parent: currentFolder}
 						if number, err := strconv.Atoi(parts[0]); err == nil {
 							currFile.Size = number
 						}
-						result[currentFolder].Children[parts[1]] = currFile
+						currentFolder.Children[currFile.Name] = currFile
 					}
 				}
-
 			}
 		}
 	}
-	return result
+	return result, nil
+}
+
+func findFolderSizes(file *File) int {
+
+	if file.isFile {
+		return file.Size
+	}
+	size := 0
+	for childFileKey := range file.Children {
+		childFile := file.Children[childFileKey]
+
+		if childFile.isFile {
+			size += childFile.Size
+		} else {
+			folderSize := findFolderSizes(&childFile)
+			if entry, ok := file.Children[childFileKey]; ok {
+				entry.Size = folderSize
+				file.Children[childFileKey] = entry
+			}
+			size += folderSize
+		}
+	}
+	file.Size = size
+
+	return size
 }
 
 func Part1(inputData []byte) int {
 
-	tree := parseInput(inputData)
+	tree, err := parseInput(inputData)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
 
-	fmt.Println(tree)
-	return 0
+	findFolderSizes(&tree)
+
+	//queue
+	queue := []File{}
+	queue = append(queue, tree)
+	atMostSize := 100000
+	res := 0
+	var file File
+	for len(queue) > 0 {
+		file, queue = queue[0], queue[1:]
+		if !file.isFile && file.Size < atMostSize {
+			res += file.Size
+		}
+		for _, items := range file.Children {
+			if !items.isFile {
+				queue = append(queue, items)
+			}
+		}
+
+	}
+
+	return res
 }
 
 func Part2(inputData []byte) int {
-	return 0
+	tree, err := parseInput(inputData)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+
+	findFolderSizes(&tree)
+
+	spaceToClean := 30000000 - (70000000 - tree.Size)
+	if spaceToClean < 0 {
+		fmt.Println("Error to clean negative")
+		return 0
+	}
+
+	//queue
+	queue := []File{}
+	queue = append(queue, tree)
+	res := 0
+	delta := spaceToClean
+	var file File
+	for len(queue) > 0 {
+		file, queue = queue[0], queue[1:]
+		if !file.isFile && file.Size > spaceToClean {
+			if file.Size-spaceToClean < delta {
+				res = file.Size
+				delta = file.Size - spaceToClean
+			}
+		}
+		for _, items := range file.Children {
+			if !items.isFile {
+				queue = append(queue, items)
+			}
+		}
+
+	}
+
+	return res
 }
 
 func main() {
@@ -86,6 +171,6 @@ func main() {
 		return
 	}
 
-	fmt.Println(Part1(inputData))
-	//fmt.Println(Part2(inputData))
+	//fmt.Println(Part1(inputData))
+	fmt.Println(Part2(inputData))
 }
