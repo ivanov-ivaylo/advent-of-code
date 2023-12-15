2b5cc53dd14eade7700cbccb2cb7e23abcfaecbe
Ivaylo Ivanov <ivaylo.ivanov@atia.com>
2023-12-15 18:53:40 +0200

    add day 10

---
+++ 2015/day10/day10.php
@@ -0,0 +1,36 @@
+<?php
+
+$input = file_get_contents("input.txt");
+
+for ($i = 0; $i < 50; $i++) {
+    
+    $numCnt = 0;
+    $numStr = "";
+    $res = "";
+    $len = strlen($input);
+    for ($s = 0; $s < $len; $s++) {
+        if ($numStr == "") {
+            $numStr = $input[$s];
+            $numCnt = 1;
+        } else if ($numStr == $input[$s]) {
+            $numCnt++;
+        } else {
+            $res = $res . $numCnt . $numStr;
+            $numStr = $input[$s];
+            $numCnt = 1;
+        }
+    }
+    if ($numCnt > 0) {
+        $res = $res . $numCnt . $numStr;
+    }
+    $input = $res;
+    
+    echo $i . " " . strlen($input) . "\n";
+    
+    
+}
+
+
+//echo $input . "\n";
+
+echo strlen($input);
