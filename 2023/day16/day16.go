4c0cd265fd7eb8e98aeb1f8619d057c1742dcd10
Ivaylo Ivanov <ivaylo.ivanov@outdoorsy.co>
2023-12-16 14:46:12 +0200

    day 16 part 1

--- 2023/day16/day16.go
+++ 2023/day16/day16.go
@@ -3,10 +3,104 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
+type Tile struct {
+	Direction string
+	X         int
+	Y         int
+}
+
+func parseInput(inputData []byte) [][]string {
+	inputDataRows := strings.Split(string(inputData), "\n")
+
+	result := [][]string{}
+	for _, dataRow := range inputDataRows {
+		row := []string{}
+		for _, charItem := range dataRow {
+			row = append(row, string(charItem))
+		}
+		result = append(result, row)
+	}
+	return result
+}
+
+func getStrCoordinatesWithDirection(tile Tile) string {
+	return strconv.Itoa(tile.X) + ":" + strconv.Itoa(tile.Y) + ":" + tile.Direction
+}
+
+func getStrCoordinates(tile Tile) string {
+	return strconv.Itoa(tile.X) + ":" + strconv.Itoa(tile.Y)
+}
+
 func Part1(inputData []byte) int {
-	return 0
+
+	grid := parseInput(inputData)
+	encountered := map[string]int{}
+	visited := map[string]int{}
+
+	queue := []Tile{{Direction: "E", X: 0, Y: 0}}
+	var currTile Tile
+	for len(queue) > 0 {
+		currTile, queue = queue[0], queue[1:]
+
+		if currTile.X < 0 || currTile.Y < 0 || currTile.X >= len(grid) || currTile.Y >= len(grid[0]) {
+			continue
+		}
+		if _, ok := encountered[getStrCoordinatesWithDirection(currTile)]; ok {
+			continue
+		}
+		encountered[getStrCoordinatesWithDirection(currTile)] = 1
+
+		c := grid[currTile.X][currTile.Y]
+		visited[getStrCoordinates(currTile)] = 1
+
+		if c == "|" && (currTile.Direction == "E" || currTile.Direction == "W") {
+			queue = append(queue, Tile{Direction: "N", X: currTile.X - 1, Y: currTile.Y})
+			queue = append(queue, Tile{Direction: "S", X: currTile.X + 1, Y: currTile.Y})
+			continue
+		}
+		if c == "-" && (currTile.Direction == "N" || currTile.Direction == "S") {
+			queue = append(queue, Tile{Direction: "E", X: currTile.X, Y: currTile.Y + 1})
+			queue = append(queue, Tile{Direction: "W", X: currTile.X, Y: currTile.Y - 1})
+			continue
+		}
+		if c == "/" {
+			if currTile.Direction == "N" {
+				queue = append(queue, Tile{Direction: "E", X: currTile.X, Y: currTile.Y + 1})
+			} else if currTile.Direction == "E" {
+				queue = append(queue, Tile{Direction: "N", X: currTile.X - 1, Y: currTile.Y})
+			} else if currTile.Direction == "W" {
+				queue = append(queue, Tile{Direction: "S", X: currTile.X + 1, Y: currTile.Y})
+			} else if currTile.Direction == "S" {
+				queue = append(queue, Tile{Direction: "W", X: currTile.X, Y: currTile.Y - 1})
+			}
+		} else if c == "\\" {
+			if currTile.Direction == "N" {
+				queue = append(queue, Tile{Direction: "W", X: currTile.X, Y: currTile.Y - 1})
+			} else if currTile.Direction == "E" {
+				queue = append(queue, Tile{Direction: "S", X: currTile.X + 1, Y: currTile.Y})
+			} else if currTile.Direction == "W" {
+				queue = append(queue, Tile{Direction: "N", X: currTile.X - 1, Y: currTile.Y})
+			} else if currTile.Direction == "S" {
+				queue = append(queue, Tile{Direction: "E", X: currTile.X, Y: currTile.Y + 1})
+			}
+		} else {
+			if currTile.Direction == "N" {
+				queue = append(queue, Tile{Direction: currTile.Direction, X: currTile.X - 1, Y: currTile.Y})
+			} else if currTile.Direction == "E" {
+				queue = append(queue, Tile{Direction: currTile.Direction, X: currTile.X, Y: currTile.Y + 1})
+			} else if currTile.Direction == "W" {
+				queue = append(queue, Tile{Direction: currTile.Direction, X: currTile.X, Y: currTile.Y - 1})
+			} else if currTile.Direction == "S" {
+				queue = append(queue, Tile{Direction: currTile.Direction, X: currTile.X + 1, Y: currTile.Y})
+			}
+		}
+	}
+
+	return len(visited)
 }
 
 func Part2(inputData []byte) int {
